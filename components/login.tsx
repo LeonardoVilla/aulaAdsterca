d4691d79f4813be438a6832ed6cd74cd55a06531
Leonardo da Silva Villa <leosivi@gmail.com>
2026-03-31 19:13:17 -0400

    Atualização do projeto 31-03-26

--- components/login.tsx
+++ components/login.tsx
@@ -1,81 +1,129 @@
 import {
-    View, 
-    Text, 
-    TextInput, 
+    View,
+    Text,
+    TextInput,
     TouchableOpacity,
     StyleSheet
 } from 'react-native';
 
 import { useState } from "react";
 import Toast from 'react-native-toast-message';
+
+import { supabase } from '../lib/supabase';
+
 //npm install react-native-toast-message
 
-export default function Login(){
+export default function Login() {
 
     const [usuario, setUsuario] = useState("");
     const [senha, setSenha] = useState("");
+    const [loading, setLoading] = useState(false)
 
-    const validaLogin = () =>{
-        if(usuario == "admin" && senha == "admin"){
-            alert("Sucesso!");
-            Toast.show({
-                type: 'success',
-                text1: 'Sucesso!',
-                text2: 'Campeão Vencedor!'
-            });
-        }else{
-            alert("Usuário ou senha inválidos!")
+    // const validaLogin = () =>{
+    //     if(usuario == "admin" && senha == "admin"){
+    //         alert("Sucesso!");
+    //         Toast.show({
+    //             type: 'success',
+    //             text1: 'Sucesso!',
+    //             text2: 'Campeão Vencedor!'
+    //         });
+    //     }else{
+    //         alert("Usuário ou senha inválidos!")
+    //         Toast.show({
+    //             type: 'error',
+    //             text1: 'Erro!',
+    //             text2: 'Usuário ou senha inválidos.'
+    //         })
+    //     }
+    // }
+
+    async function signInWithEmail() {
+        setLoading(true)
+        const { error } = await supabase.auth.signInWithPassword({
+            email: usuario,
+            password: senha,
+        })
+        if (error){
             Toast.show({
                 type: 'error',
                 text1: 'Erro!',
                 text2: 'Usuário ou senha inválidos.'
             })
         }
+        setLoading(false)
+    }
+    async function signUpWithEmail() {
+        setLoading(true)
+        const {
+            data: { session },
+            error,
+        } = await supabase.auth.signUp({
+            email: usuario,
+            password: senha,
+        })
+        if (error) {
+            Toast.show({
+                type: 'error',
+                text1: 'Erro!',
+                text2: 'Usuário ou senha inválidos.'
+            })
+        }
+        if (!session) {
+            Toast.show({
+                type: 'error',
+                text1: 'Erro!',
+                text2: 'Usuário ou senha inválidos.'
+            })
+        }
+        setLoading(false)
     }
 
-    return(
+    return (
         <View style={styles.container}>
             <Text style={styles.titulo}>Área Restrita</Text>
-            
-            <TextInput 
-                style={styles.input} 
+
+            <TextInput
+                style={styles.input}
                 value={usuario}
                 onChangeText={setUsuario}
             />
 
-            <TextInput 
-                style={styles.input} 
-                secureTextEntry={true} 
+            <TextInput
+                style={styles.input}
+                secureTextEntry={true}
                 value={senha}
                 onChangeText={setSenha}
             />
-            
-            <TouchableOpacity style={styles.botao} onPress={validaLogin}>
+            <TouchableOpacity
+                onPress={() => signInWithEmail()}
+                disabled={loading}
+            ></TouchableOpacity>
+            <TouchableOpacity style={styles.botao} onPress={signUpWithEmail}>
                 <Text style={styles.titulo}>Fazer Login</Text>
             </TouchableOpacity>
 
             <Toast />
-            
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         backgroundColor: 'pink',
         alignItems: 'center',
         justifyContent: 'center',
         padding: 20,
     },
-    titulo:{
+    titulo: {
         fontFamily: 'verdana',
         marginBottom: 20,
         fontSize: 24,
         color: '#ffffff',
         fontWeight: 'bold',
     },
-    input:{
+    input: {
         width: '100%',
         height: 45,
         backgroundColor: '#ffffff',
@@ -83,7 +131,7 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         marginBottom: 20,
     },
-    botao:{
+    botao: {
         width: '100%',
         height: 45,
         backgroundColor: 'green',
