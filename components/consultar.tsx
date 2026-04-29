2f2f594839dbf9e2ffee5dd7aa3ab7903dfab315
Leonardo da Silva Villa <leosivi@gmail.com>
2026-04-28 21:27:18 -0400

    Atualização do projeto dia 28-04-2026

---
+++ components/consultar.tsx
@@ -0,0 +1,48 @@
+import { supabase } from "@/lib/supabase";
+import { useEffect, useState } from "react";
+import { useIsFocused } from "@react-navigation/native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+
+export default function Consultar(){
+    const [alunos, setAlunos] = useState<any[]>([]);
+    const isFocused = useIsFocused();
+
+    useEffect(() => {
+        if(isFocused){
+            carregarAlunos();
+        }
+    }, [isFocused]);
+    
+    async function carregarAlunos(){
+        const {data, error } = await supabase
+        .from('alunos')
+        .select('*');
+
+        setAlunos(data || []);
+    }
+
+    return(
+        <View style={styles.container}>
+            <Text>Consultar Alunos</Text>
+            <FlatList
+                data={alunos}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({item}) =>(
+                    <View>
+                        <Text>{item.nome}</Text>
+                        <Text>{item.idade}</Text>
+                        <Text>{item.email}</Text>
+                    </View>
+                )}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#fff',
+        color: '#000',
+    }
+})
